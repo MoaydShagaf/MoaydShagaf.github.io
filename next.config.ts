867e925817a4b0990acda66f7faf7021e1183afa
MoaydShagaf <moaydshagaf3@gmail.com>
2025-02-20 12:01:12 +0200

    fixed for build

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
-  output: 'export',
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  distDir: "build",
+  output: "export",
   images: {
-    unoptimized: true, // Required for static export if using images
+    unoptimized: true,
   },
 };
 
-export default nextConfig
+module.exports = nextConfig;
